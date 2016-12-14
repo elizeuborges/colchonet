1e418414fd2a5128364543fd3fdfb3902d031c39
elizeuborges <elizeuborges@hotmail.com>
2016-12-13 22:15:23 -0200

    primeiro commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates_presence_of :email, :full_name, :location, :password
+    validates_confirmation_of :password
+    validates_length_of :bio, :minimum => 10, :allow_blank => false
+end
